36f2a0a484f7fb51e061f0dd1e06f68e2d5d05c9
junior7819 <crackleo507@gmail.com>
2026-03-04 00:08:17 -0300

    Create gpu_runner.py

---
+++ .github/script/gpu_runner.py
@@ -0,0 +1,9 @@
+import os
+import torch
+
+print("Kaggle CI iniciado")
+
+print("GPU disponible:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("GPU:", torch.cuda.get_device_name(0))
